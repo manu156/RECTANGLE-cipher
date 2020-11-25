86b322b9ce1e3e2bbb742d8c5dede84bb3610a9f
manu <10695678+manu156@users.noreply.github.com>
2020-11-26 00:29:25 +0530

    added file

---
+++ main.rs
@@ -0,0 +1,101 @@
+/*
+ * RECTANGLE
+ *
+ */
+
+use std::convert::TryInto;
+const SBOX: [u16; 16] = [6, 5, 0xc, 0xa, 1, 0xe, 7, 9, 0xb, 0, 3, 0xd, 8, 0xf, 4, 2];
+const RC: [u16; 25] = [0x01, 0x02, 0x04, 0x09, 0x12, 0x05, 0x0B, 0x16, 0x0C, 0x19, 0x13, 0x07, 0x0F, 0x1F, 0x1E, 0x1C, 0x18, 0x11, 0x03, 0x06, 0x0D, 0x1B, 0x17, 0x0E, 0x1D];
+
+fn main() {
+    // let mut key_iv: [u16; 5] = [0; 5];
+    // next_subkey(&mut key_iv, 0);
+    // println!("{:?}", key_iv);
+    println!("{:b}", cipher(!0, !0));
+}
+
+fn sbox(input: u16) -> u16 {
+    SBOX[input as usize]
+}
+
+fn next_subkey(initial: &mut [u16; 5], round: usize) {
+    // apply sbox
+    for i in 0..4 {
+        let sb = sbox(((initial[0] >> i) & 1) | ((initial[1] >> i) & 1) << 1 | ((initial[2] >> i) & 1) << 2 | ((initial[3] >> i) & 1) << 3);
+        for k in 0..4 {
+            initial[k] = (initial[k] & !(0b1 << i)) | ((sb >> k) & 1) << i;
+        }
+    }
+
+    // apply Feistal transformation
+    let row0_copy: u16 = initial[0];
+    initial[0] = ((initial[0] >> 8) | (initial[0] << 8)) ^ initial[1];
+    initial[1] = initial[2];
+    initial[2] = initial[3];
+    initial[3] = ((initial[3] >> 4) | (initial[3] << 12)) ^ initial[4];
+    initial[4] = row0_copy;
+
+    // add 5-bit round constant
+    initial[0] = initial[0] ^ RC[round];
+}
+
+fn add_round_key(input: &mut [u16; 4], k: [u16; 4]) {
+    for i in 0..4 {
+        input[i] = input[i] ^ k[i];
+    }
+}
+
+fn sub_column(input: &mut [u16; 4]) {
+    let mut t: [u16; 12] = [0; 12];
+    let mut b: [u16; 4] = [0; 4];
+
+    t[1] = !input[1];
+    t[2] = input[0] & t[1];
+    t[3] = input[2] ^ input[3];
+    b[0] = t[2] ^ t[3];
+
+    t[5] = input[3] | t[1];
+    t[6] = input[0] ^ t[5];
+    b[1] = input[2] ^ t[6];
+    t[8] = input[1] ^ input[2];
+
+    t[9] = t[3] & t[6];
+    b[3] = t[8] ^ t[9];
+    t[11] = b[0] | t[8];
+    b[2] = t[6] ^ t[11];
+
+    for i in 0..4 {
+        input[i] = b[i];
+    }
+}
+
+fn shift_row(input: &mut [u16; 4]) {
+    input[1] = (input[1] << 1) | (input[1] >> 15);
+    input[2] = (input[2] << 12) | (input[2] >> 4);
+    input[3] = (input[3] << 13) | (input[3] >> 3);
+}
+
+fn cipher(input: u64, iv: u128) -> u64 {
+    let mut key: [u16; 5] = [
+        (iv & 0xffff) as u16,
+        ((iv >> 16) & 0xffff) as u16,
+        ((iv >> 32) & 0xffff) as u16,
+        ((iv >> 48) & 0xffff) as u16,
+        ((iv >> 64) & 0xffff) as u16
+    ];
+    let mut state: [u16; 4] = [
+        (input & 0xffff) as u16,
+        ((input >> 16) & 0xffff) as u16,
+        ((input >> 32) & 0xffff) as u16,
+        ((input >> 48) & 0xffff) as u16,
+    ];
+
+    for i in 0..25 {
+        add_round_key(&mut state, key[0..4].try_into().expect(""));
+        sub_column(&mut state);
+        shift_row(&mut state);
+        next_subkey(&mut key, i);
+    }
+    add_round_key(&mut state, key[0..4].try_into().expect(""));
+    (state[0] as u64) | ((state[1] as u64) << 16) | ((state[2] as u64) << 32) | ((state[3] as u64) << 48)
+}
